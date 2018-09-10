d7bf80fccf81c62d56c82a80f7b6000de412fb56
grzegorztata <grzegorz.tata@gmail.com>
2018-09-10 20:08:21 +0200

    zad 14.4

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -16,18 +16,27 @@ public class TaskController {
         return new ArrayList<>();
     }
 
+    @RequestMapping(method = RequestMethod.GET, value = "getTask")
     public TaskDto getTask(Long taskId) {
         return new TaskDto(1L, "test title", "test content");
     }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "deleteTask")
     public void deleteTask(Long taskId){
 
     }
 
+    @RequestMapping(method = RequestMethod.PUT, value = "updateTask")
     public TaskDto updateTask(TaskDto taskDto) {
         return new TaskDto(1L, "Edited test title", "Test content");
     }
 
+    @RequestMapping(method = RequestMethod.POST, value = "createTask")
     public void createTask(TaskDto taskDto) {
 
     }
 }
+
+//https://stackoverflow.com/questions/46176688/spring-request-mapping-post-vs-put-same-method-same-logic-but
+//update - PUT
+//create - POST
