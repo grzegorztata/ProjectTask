d0193b729b4dad05dfe12135c1ec53bed9efa60e
grzegorztata <grzegorz.tata@gmail.com>
2018-11-04 01:41:25 +0200

    19.2

--- src/main/java/com/crud/tasks/config/CoreConfiguration.java
+++ src/main/java/com/crud/tasks/config/CoreConfiguration.java
@@ -30,9 +30,10 @@ public class CoreConfiguration implements WebMvcConfigurer {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.crud.tasks.controller"))
                 .paths(PathSelectors.any())
                 .build();
+        //https://itproblemy.pl/questions/48250231/how-to-limit-the-project-scan-only-to-the-comhomecontroller-package-swagger
     }
     @Override
     public void addResourceHandlers(final ResourceHandlerRegistry registry) {
